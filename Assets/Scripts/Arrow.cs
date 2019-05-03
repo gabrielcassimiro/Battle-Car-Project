63719b3039257aa0eb03dcb95161db4110a143dd
Gabriel Cassimiro <gdinizcassimiro@gmail.com>
2019-05-03 08:50:12 -0300

    Primeiro commit projeto

---
+++ Assets/Scripts/Arrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour
+{
+    [SerializeField] private float RotateSpeed = 300f;
+    [SerializeField] private bool IsHitSomething;
+    private Rigidbody rb;
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        if(!IsHitSomething){
+            transform.rotation = Quaternion.LookRotation(rb.velocity)
+                * Quaternion.Euler(Vector3.right * 90)
+                * Quaternion.Euler(Vector3.up * Time.time * RotateSpeed);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if(!other.collider.CompareTag("Bullet")){
+            IsHitSomething = true;
+            rb.constraints = RigidbodyConstraints.FreezeAll;
+            StartCoroutine(destroyCoroutine());
+        }
+    }
+    
+    private IEnumerator destroyCoroutine(){
+        yield return new WaitForSeconds(2f);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/ArrowShoot.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowShoot : MonoBehaviour
+{
+    [Header("Input Name")]
+    [SerializeField] private string m_InputFire;
+    
+    [Header("Instantiate Arrow")]
+     [SerializeField]
+    private Rigidbody m_ArrowPrefab;
+
+    [SerializeField]
+    private Transform m_ArrowSpawn;
+
+    [SerializeField]
+    private float m_MinForce = 10.0f;
+
+    [SerializeField]
+    private float m_MaxForce = 50.0f;
+
+    [SerializeField]
+    private float m_TimeToMaxForce = 2.0f;
+
+    [SerializeField]
+    private float m_CurrentForce;
+
+    private float m_ElapsedTime;
+
+    [SerializeField]
+    private float m_timeToBullet = 0;
+    [SerializeField]
+    private float m_BulletReady;
+    [SerializeField]
+    private bool Fire;
+
+    public void Update()
+    {
+        m_timeToBullet += Time.deltaTime;
+
+        if (Input.GetButton(m_InputFire) && m_timeToBullet > m_BulletReady)
+        {
+            m_CurrentForce = Mathf.Lerp(m_MinForce, m_MaxForce, m_ElapsedTime / m_TimeToMaxForce);
+            m_ElapsedTime += Time.deltaTime;
+            Fire = true;
+        }
+
+        if (Input.GetButtonUp(m_InputFire) && Fire)
+        {
+            Fire = false;
+            m_timeToBullet = 0;
+            m_ElapsedTime = 0.0f;
+            Rigidbody arrow = Instantiate<Rigidbody>(m_ArrowPrefab, m_ArrowSpawn.position, m_ArrowSpawn.rotation);
+            arrow.AddForce(m_ArrowSpawn.up * m_CurrentForce, ForceMode.VelocityChange);
+        }
+    }
+}
